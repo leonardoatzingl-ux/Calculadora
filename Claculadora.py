807559b3ae418eeac94166f47224facf524c62ed
leonardoatzingl-ux <leonardoatzingl@isb.edu.mx>
2025-09-03 09:23:34 -0600

    Se sube practica de calculadora

---
+++ Claculadora.py
@@ -0,0 +1,15 @@
+#Calculadora
+num1=float("Ingresar el primer numero")
+Num2=float("Ingresar el segundo numero")
+
+suma=num1+Num2
+resta=num1-Num2
+multiplicación=num1*Num2
+divición=num1/Num2
+
+
+print("El resultado de la suma es: ",suma)
+print("El resultado de la resta es: ",resta)
+print("El resultado de la multiplicación es: ",multiplicación)
+print("El resultado de la divición es: ",divición)
+
