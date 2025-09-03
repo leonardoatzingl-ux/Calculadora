2da9e9ae73f54aaa05c2f27f4fabc51dc50bfedb
leonardoatzingl-ux <leonardoatzingl@isb.edu.mx>
2025-09-03 12:18:58 -0600

    Se sube practica calculadora 2

--- Claculadora.py
+++ Claculadora.py
@@ -5,11 +5,38 @@ Num2=float("Ingresar el segundo numero")
 suma=num1+Num2
 resta=num1-Num2
 multiplicación=num1*Num2
-divición=num1/Num2
+división=num1/Num2
 
 
 print("El resultado de la suma es: ",suma)
 print("El resultado de la resta es: ",resta)
 print("El resultado de la multiplicación es: ",multiplicación)
-print("El resultado de la divición es: ",divición)
+print("El resultado de la divición es: ",división)
 
+#Mostrar menu de operaciones
+print("\nElige la operación que deseas realizar:")
+print("1. suma")
+print("2. resta")
+print("e. multiplicación")
+print("4 divsión") 
+
+opcion = input("Ingresa el numero de la opción:")
+
+#Evaluar la opción seleccionada
+if opcion == "1":
+    resultado = num1 + Num2
+    print("El resultado de la suma es:", resultado)
+elif opcion == "2":
+    resultado = num1 - Num2
+    print("El resultado de la resta es:", resultado)
+elif opcion == "3":
+    resultado = num1 * Num2
+    print("El resultado de la mutiplicaión es:", resultado)
+elif opcion == "4":
+    if Num2 !=0: #Evitar división entre cero
+        resultado = num1 / Num2
+        print("El resultado de la división es:", resultado)
+    else:
+        print("Error: no se puede dividir entre cero")
+else:
+    print("Opción no válida") 
